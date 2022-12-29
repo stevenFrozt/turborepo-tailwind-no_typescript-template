c8bea172042de56c1db266f4bf4bf88daaa26110
stevenFrozt <steven.evio@gmail.com>
2022-12-29 23:07:06 +0800

    added Tailwind

--- packages/components/Button.js
+++ packages/components/Button.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from "react"
 export const Button = () => {
-  return <button>Boop</button>;
-};
+  return <button className="bg-blue-500">Boop</button>
+}
